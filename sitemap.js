30b569a87eec1c44d7b485ba651f293b55aef211
1000hoursandamap <oscarveloria.jr@gmail.com>
2016-12-01 10:03:54 -0700

    Create sitemap.js

---
+++ sitemap.js
@@ -0,0 +1,18 @@
+buildMap(document.getElementById('sitemapWrapper')){
+<ul>
+ <li> <a href="https://perryma.tk/1000hoursandamap/index.html>Home</a> </li>
+ <li> <a href="https://perryma.tk/1000hoursandamap/bio.html>Biography</a> </li>
+ <li> Class Projects
+  <ul> 
+    <li><a href="https://perryma.tk/1000hoursandamap/thoughts.html">SPANTAG and JScript Events</a></li>
+    <li><a href="https://perryma.tk/1000hoursandamap/game.html">JavaScript Game</a></li>
+    <li><a href="https://perryma.tk/1000hoursandamap/popup.html">POPUP</a></li>
+    <li><a href="https://perryma.tk/1000hoursandamap/htmltest.html">ModifyingW/Javascript</a></li>
+    <li><a href="https://perryma.tk/1000hoursandamap/todolist.html">Creating Elements</a></li>
+    <li><a href="https://perryma.tk/1000hoursandamap/chalkboard.html">ChalkBoard</a></li>
+    <li><a href="https://perryma.tk/1000hoursandamap/tictactoe.html">TicTacToe</a></li>
+    <li><a href="https://perryma.tk/1000hoursandamap/tablex.html">Multiplication Table</a></li>
+  </ul>
+ </li>
+</ul>
+}
